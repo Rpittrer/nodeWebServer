e8eddefdc90a11001e0b84002683164940651868
Arpit <rpittrer@gmail.com>
2018-06-22 17:16:57 +0530

    express-gooood

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Express!</h1>');
+    res.send('hello world');
 });
 
 app.get('/about', (req, res) => {
@@ -20,4 +21,6 @@ app.get('/bad', (req, res) => {
     res.send(error);
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is up on port 3000");
+});
