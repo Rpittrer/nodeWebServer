8816fd73d9816d5ab83e14545578e4cb56de2d3f
Arpit <rpittrer@gmail.com>
2018-06-24 03:22:30 +0530

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.port || 3000;
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials/');
@@ -22,9 +23,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintainence.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintainence.hbs');
+// });
 
 app.get('/', (req, res) => {
     res.send('hello world');
@@ -48,6 +49,6 @@ app.get('/bad', (req, res) => {
     res.send(error);
 });
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
