9368f2bd3ca9078e33f6bbed8d2a04545a18bbd0
Arpit <rpittrer@gmail.com>
2018-06-24 01:30:04 +0530

    footer

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
@@ -12,7 +14,9 @@ app.get('/about', (req, res) => {
         name: 'arpit',
         likes: ['blender', 'node']
     };
-    res.send(about);
+    res.render('about.hbs', {
+        titleName: 'Good Morning',
+    });
 });
 app.get('/bad', (req, res) => {
     var error = {
