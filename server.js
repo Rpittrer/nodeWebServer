f750191b07623a8a5933b933a8935b3ff387eeca
Arpit <rpittrer@gmail.com>
2018-06-24 02:31:19 +0530

    middleware

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
+hbs.registerPartials(__dirname + '/views/partials/');
+
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('currentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log("Unable to write log");
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainence.hbs');
+});
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/about', (req, res) => {
     var about = {
